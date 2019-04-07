9e206394b51f47242715bbf56b4efd6801eaf3bb
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-04-07 15:02:45 +0900

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by 神原良継 on 2019/04/07.
+//  Copyright © 2019 YoshitsuguKambara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
